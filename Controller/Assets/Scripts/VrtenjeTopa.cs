28607053d69afa0a4fbe86ae8bbf9d9bc67bc644
Saso <saso.ivic@student.um.si>
2018-10-17 19:33:47 +0200

    Controller and plaer movement

---
+++ Controller/Assets/Scripts/VrtenjeTopa.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrtenjeTopa : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+       
+            var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+	}
+}
